fe324f29625f09b028d34e69db022d0d815c1d86
NXTNiklas <40755900+NXTNiklas@users.noreply.github.com>
2018-07-03 14:25:19 +0200

    Added methods.py for Librepilot support on CC3D
    If you want to connect CC3D to your RPI use the Config named in the Readme!
    It's important to stop the UAVTalk at the end of the Script, because elseway ctr+c won't work!

--- droneController/methods.py
+++ droneController/methods.py
@@ -38,6 +38,7 @@ import datetime
 import time
 import math
 import json
+import threading    
  
 
 from thread import start_new_thread
@@ -45,15 +46,15 @@ from librepilot.uavtalk.uavobject import *
 from librepilot.uavtalk.uavtalk import *
 from librepilot.uavtalk.objectManager import *
 from librepilot.uavtalk.connectionManager import *
-from BufferedSerialReader import BufferedReader
+from bufferedSerialReader import BufferedSerialReader
 
 def _hex02(value):
     return "%02X" % value
 
 class UAVTalkDemo():
     def __init__(self,ws):
-	    self.__ws = ws
-		self.__armed = False
+        self.__ws = ws
+        self.__armed = False
         self.nbUpdates = 0
         self.lastRateCalc = time.time()
         self.updateRate = 0
@@ -76,16 +77,15 @@ class UAVTalkDemo():
         
     def eprint(self,message):
         print message
-        self.__ws.write_message(json.dumps(message))
+        #self.__ws.write_message(json.dumps(message))
         
     def terminate(self):
         self.__running = False
 
     
 
-    def setup(self, ws, port):
+    def setup(self, port):
         # type: (WebSocket, int) ->
-        self.__ws = ws
         self.eprint("Opening Port \"%s\"" % port)
         if port[:3].upper() == "COM":
             _port = int(port[3:]) - 1
@@ -115,6 +115,7 @@ class UAVTalkDemo():
         self.eprint("Getting all Data")
         self.objMan.requestAllObjUpdate()
         self.__setuped = True
+        self.eprint("Complete!")
 
     # self.eprint("SN:",
     # sn = self.objMan.FirmwareIAPObj.CPUSerial.value
@@ -122,7 +123,7 @@ class UAVTalkDemo():
 
     def stop(self):
         try:
-           if self.uavTalk:
+            if self.uavTalk:
                self.eprint("Stopping UavTalk")
                self.uavTalk.stop()
         except AttributeError:
@@ -202,198 +203,199 @@ class UAVTalkDemo():
         return distanz
 
     def initUltrasonic(self):
-	self.uPort = none
-	port = "/dev/ttyUSB0"
-	print "Opening Port \"%s\"" % port
-	if port[:3].upper() == "COM":
-    	    _port = int(port[3:]) - 1
-	else:
-    	    _port = port
+        self.uPort = none
+        port = "/dev/ttyUSB0"
+        print "Opening Port \"%s\"" % port
+        if port[:3].upper() == "COM":
+                _port = int(port[3:]) - 1
+        else:
+                _port = port
+    
+        self.uPort = serial.Serial(_port, 115200, timeout=.5)
+        if not self.uPort.isOpen():
+            raise IOError("Failed to open serial port")
+        return serPort
 
-	self.uPort = serial.Serial(_port, 115200, timeout=.5)
-	if not self.uPort.isOpen():
- 	    raise IOError("Failed to open serial port")
-	return serPort
-
-    def getUSensor(self,index)
-	buffer = ""
-	if not self.uPort.isOpen():
-	    self.initUltrasonic
-	actual = self.uPort.read()
-	if(actual = "\n"):
-	    splitted=buffer.split(" ")
-	    return splitted[index]
-	else:
-	    buffer+=actual
-	
+    def getUSensor(self,index):
+        buffer = ""
+        if not self.uPort.isOpen():
+            self.initUltrasonic
+        actual = self.uPort.read()
+        if(actual == "\n"):
+            splitted=buffer.split(" ")
+            return splitted[index]
+        else:
+            buffer+=actual
+    
     def getBottumUSensor(self):
-	value = self.getUSensor(self,0)
-	return value
+        value = self.getUSensor(self,0)
+        return value
 
     def setControlValues(self, throttle, pitch, roll, yaw):
-            self.eprint(str(throttle) + " " + str(pitch) + " " + str(roll) + " " + str(yaw))
-            self.eprint("In ControlFunction!")
-            if throttle > 1.0:
-                self.eprint("throttle return" + str(throttle))
-                return -1
-            if pitch > 1.0:
-                self.eprint("pitch return")
-                return -1
-            if yaw > 1.0:
-                self.eprint("yaw return")
-                return -1
-            if roll > 1.0:
-                self.eprint("roll return")
-                return -1
-            if self.bBoardInitialized == False:
-                self.eprint("Board not armed - arming Board...")
-                self.__takeControl()
-                self.__armBoard()
-                self.bBoardInitialized = True
-            
+        self.eprint(str(throttle) + " " + str(pitch) + " " + str(roll) + " " + str(yaw))
+        self.eprint("In ControlFunction!")
+        if throttle > 1.0:
+            self.eprint("throttle return" + str(throttle))
+            return -1
+        if pitch > 1.0:
+            self.eprint("pitch return")
+            return -1
+        if yaw > 1.0:
+            self.eprint("yaw return")
+            return -1
+        if roll > 1.0:
+            self.eprint("roll return")
+            return -1
+        if self.bBoardInitialized == False:
+            self.eprint("Board not armed - arming Board...")
             self.__takeControl()
-            self.eprint("taking over control")            
-            self.objMan.ManualControlCommand.Thrust.value = throttle
-            self.objMan.ManualControlCommand.Pitch.value = pitch
-            self.objMan.ManualControlCommand.Roll.value = yaw
-            self.objMan.ManualControlCommand.Yaw.value = roll
-            self.objMan.ManualControlCommand.updated()
-            
-            # just for debugging
-            #time.sleep(1)
-            #self.objMan.ManualControlCommand.Thrust.value = 0
-            #self.objMan.ManualControlCommand.Pitch.value = 0
-            #self.objMan.ManualControlCommand.Roll.value = 0
-            #self.objMan.ManualControlCommand.Yaw.value = 0
-            #self.objMan.ManualControlCommand.updated()
-            #self.__freeControl()
+            self.__armBoard()
+            self.bBoardInitialized = True
+        
+        self.__takeControl()
+        self.eprint("taking over control")            
+        self.objMan.ManualControlCommand.Thrust.value = throttle
+        self.objMan.ManualControlCommand.Pitch.value = pitch
+        self.objMan.ManualControlCommand.Roll.value = yaw
+        self.objMan.ManualControlCommand.Yaw.value = roll
+        self.objMan.ManualControlCommand.updated()
+        
+        # just for debugging
+        #time.sleep(1)
+        #self.objMan.ManualControlCommand.Thrust.value = 0
+        #self.objMan.ManualControlCommand.Pitch.value = 0
+        #self.objMan.ManualControlCommand.Roll.value = 0
+        #self.objMan.ManualControlCommand.Yaw.value = 0
+        #self.objMan.ManualControlCommand.updated()
+        #self.__freeControl()
     
     def hoverTest(self, height, minThrottle, maxThrottle, duration):
-            self.eprint("Beginne AutoHover: Hoehe: " + str(height) + "cm minimal Thrust: " + minThrottle + " maximum Thrust: " + maxThrottle + " hover Zeit: " + duration + " Sekunden.")
-            timer = 10
-            self.eprint("Start in: " + str(timer))
-            while(timer > 0):
-                self.eprint(str(timer))
-                timer = timer - 1
-            self.eprint("Starte AutoHover")
-            timer = 0
-            while(timer < duration):
-                position = self.getBottomUSensor()
-                if position > 250:
-                    self.eprint("Hoehe ist groesser als 2.5m... starte neue Messung")
-                    position = self.getBottomUSensor()
-                    if position > 250:
-                        self.eprint("Hoehe ist immer noch groesser als 2.5m... starte neue Messung")
-                        position = self.getBottomUSensor()
-                        if position > 250:
-                            self.eprint("Hoehe ist immer noch groesser als 2.5m... starte Abschaltung der Motoren... MAYDAY MAYDAY MAYDAY... ")
-                            self.setControlValues(0, 0, 0, 0)
-                self.eprint("Gemessene Hoehe: " + str(position) + "cm. Verbleibende hover Zeit: " + str(duration - timer))
-                factor = height - position * 0.05
-                if factor < minThrottle:
-                    factor = minThrottle
-                    
-                if factor > maxThrottle:
-                    factor =  maxThrottle
-                self.eprint("Neu ermittelter Thrustfactor: " + str(factor) + " ... starte Anpassung")
-                self.setControlValues(factor, 0, 0, 0)
-                self.eprint("Neue Messung in 0,1 Sekunden...")
-                time.sleep(0.1)
-                timer = timer + 0.1
-
-            self.eprint("Ende des Hover Vorgangs. Leite automatische Landung ein...")
-            self.autoLand()
-            
-    def autoLand(self):
-            heigth = 0
+        self.eprint("Beginne AutoHover: Hoehe: " + str(height) + "cm minimal Thrust: " + minThrottle + " maximum Thrust: " + maxThrottle + " hover Zeit: " + duration + " Sekunden.")
+        timer = 10
+        self.eprint("Start in: " + str(timer))
+        while(timer > 0):
+            self.eprint(str(timer))
+            timer = timer - 1
+        self.eprint("Starte AutoHover")
+        timer = 0
+        while(timer < duration):
             position = self.getBottomUSensor()
             if position > 250:
-                    self.eprint("Hoehe ist groesser als 2.5m... starte neue Messung")
+                self.eprint("Hoehe ist groesser als 2.5m... starte neue Messung")
+                position = self.getBottomUSensor()
+                if position > 250:
+                    self.eprint("Hoehe ist immer noch groesser als 2.5m... starte neue Messung")
                     position = self.getBottomUSensor()
                     if position > 250:
-                        self.eprint("Hoehe ist immer noch groesser als 2.5m... starte neue Messung")
-                        position = self.getBottomUSensor()
-                        if position > 250:
-                            self.eprint("Hoehe ist immer noch groesser als 2.5m... starte Abschaltung der Motoren... MAYDAY MAYDAY MAYDAY... ")
-                            self.setControlValues(0, 0, 0, 0)
-            while(position > 10):
-                position = self.getBottomUSensor
-                self.eprint("Gemessene Hoehe: " + str(position) + "cm. Verbleibende Hoehe: " + str(position - height))
-                factor = height - position
-                factor = factor * 0.05
-                    
-                if factor < minThrottle:
-                    factor = minThrottle
-                if factor > maxThrottle:
-                    factor =  maxThrottle
-                self.eprint("Neu ermittelter Thrustfactor: " + str(factor) + " ... starte Anpassung")
-                self.setControlValues(factor, 0, 0, 0)
-                self.eprint("Neue Messung in 0,1 Sekunden...")
-                time.sleep(0.1)
-                timer = timer + 0.1
+                        self.eprint("Hoehe ist immer noch groesser als 2.5m... starte Abschaltung der Motoren... MAYDAY MAYDAY MAYDAY... ")
+                        self.setControlValues(0, 0, 0, 0)
+            self.eprint("Gemessene Hoehe: " + str(position) + "cm. Verbleibende hover Zeit: " + str(duration - timer))
+            factor = height - position * 0.05
+            if factor < minThrottle:
+                factor = minThrottle
+                
+            if factor > maxThrottle:
+                factor =  maxThrottle
+            self.eprint("Neu ermittelter Thrustfactor: " + str(factor) + " ... starte Anpassung")
+            self.setControlValues(factor, 0, 0, 0)
+            self.eprint("Neue Messung in 0,1 Sekunden...")
+            time.sleep(0.1)
+            timer = timer + 0.1
+
+        self.eprint("Ende des Hover Vorgangs. Leite automatische Landung ein...")
+        self.autoLand()
             
-            self.eprint("Landehoehe erreicht, schalte Motoren ab!")
-            self.setControlValues(0, 0, 0, 0)
+    def autoLand(self):
+        heigth = 0
+        position = self.getBottomUSensor()
+        if position > 250:
+                self.eprint("Hoehe ist groesser als 2.5m... starte neue Messung")
+                position = self.getBottomUSensor()
+                if position > 250:
+                    self.eprint("Hoehe ist immer noch groesser als 2.5m... starte neue Messung")
+                    position = self.getBottomUSensor()
+                    if position > 250:
+                        self.eprint("Hoehe ist immer noch groesser als 2.5m... starte Abschaltung der Motoren... MAYDAY MAYDAY MAYDAY... ")
+                        self.setControlValues(0, 0, 0, 0)
+        while(position > 10):
+            position = self.getBottomUSensor
+            self.eprint("Gemessene Hoehe: " + str(position) + "cm. Verbleibende Hoehe: " + str(position - height))
+            factor = height - position
+            factor = factor * 0.05
+                
+            if factor < minThrottle:
+                factor = minThrottle
+            if factor > maxThrottle:
+                factor =  maxThrottle
+            self.eprint("Neu ermittelter Thrustfactor: " + str(factor) + " ... starte Anpassung")
+            self.setControlValues(factor, 0, 0, 0)
+            self.eprint("Neue Messung in 0,1 Sekunden...")
+            time.sleep(0.1)
+            timer = timer + 0.1
+        
+        self.eprint("Landehoehe erreicht, schalte Motoren ab!")
+        self.setControlValues(0, 0, 0, 0)
+    
     def driveInput(self):
-            self.__takecontrol()
+        self.__takecontrol()
+    
+        self.__armBoard()
+    
+        self.eprint("Applying Throttle")
+    
+        # self.objMan.ManualControlCommand.Throttle.value = 0.01 # very small value for safety
+        # Assuming board do not control a helicopter, Thrust value will be equal to Throttle value.
+        # Because a 'high' value can be read from latest real RC input value, 
+        # initial value is set now to zero for safety reasons.
+        self.objMan.ManualControlCommand.Thrust.value = 0.01
+        # self.objMan.ManualControlCommand.Throttle.value = self.objMan.ManualControlCommand.Thrust.value
+        self.objMan.ManualControlCommand.updated()
+        time.sleep(0.3)
+    
+        count = 60
+        self.eprint("Moving Pitch input")
+        while (count > 0):
+            count-=1
+            if self.objMan.ManualControlCommand.Pitch.value < 1:
+                 self.objMan.ManualControlCommand.Pitch.value += 0.05
+                 self.objMan.ManualControlCommand.updated()
+                 time.sleep(0.1)
+            if self.objMan.ManualControlCommand.Pitch.value > 1:
+                self.objMan.ManualControlCommand.Pitch.value = 0
+                self.objMan.ManualControlCommand.updated()
+                time.sleep(0.1)
+    
+        self.objMan.ManualControlCommand.Pitch.value = 0
+        self.objMan.ManualControlCommand.updated()
+        time.sleep(0.5)
+    
+        count = 60
+        self.eprint("Moving Roll input")
+        while (count > 0):
+            count-=1
+            if self.objMan.ManualControlCommand.Roll.value < 1:
+                 self.objMan.ManualControlCommand.Roll.value += 0.05
+                 self.objMan.ManualControlCommand.updated()
+                 time.sleep(0.1)
+    
+            if self.objMan.ManualControlCommand.Roll.value > 1:
+                self.objMan.ManualControlCommand.Roll.value = 0
+                self.objMan.ManualControlCommand.updated()
+                time.sleep(0.1)
+    
+        self.objMan.ManualControlCommand.Roll.value = 0
+        self.objMan.ManualControlCommand.updated()
+        time.sleep(0.5)
+    
+        self.eprint("Setting Throttle to minimum")
+        self.objMan.ManualControlCommand.Throttle.value = -1
+        self.objMan.ManualControlCommand.updated()
+        time.sleep(1)
+    
+        self.__disarmBoard()
+    
+        self.__freeControl()
         
-            self.__armBoard()
-        
-            self.eprint("Applying Throttle")
-        
-            # self.objMan.ManualControlCommand.Throttle.value = 0.01 # very small value for safety
-            # Assuming board do not control a helicopter, Thrust value will be equal to Throttle value.
-            # Because a 'high' value can be read from latest real RC input value, 
-            # initial value is set now to zero for safety reasons.
-            self.objMan.ManualControlCommand.Thrust.value = 0.01
-            # self.objMan.ManualControlCommand.Throttle.value = self.objMan.ManualControlCommand.Thrust.value
-            self.objMan.ManualControlCommand.updated()
-            time.sleep(0.3)
-
-            count = 60
-            self.eprint("Moving Pitch input")
-            while (count > 0):
-                count-=1
-                if self.objMan.ManualControlCommand.Pitch.value < 1:
-                     self.objMan.ManualControlCommand.Pitch.value += 0.05
-                     self.objMan.ManualControlCommand.updated()
-                     time.sleep(0.1)
-                if self.objMan.ManualControlCommand.Pitch.value > 1:
-                    self.objMan.ManualControlCommand.Pitch.value = 0
-                    self.objMan.ManualControlCommand.updated()
-                    time.sleep(0.1)
-        
-            self.objMan.ManualControlCommand.Pitch.value = 0
-            self.objMan.ManualControlCommand.updated()
-            time.sleep(0.5)
-
-            count = 60
-            self.eprint("Moving Roll input")
-            while (count > 0):
-                count-=1
-                if self.objMan.ManualControlCommand.Roll.value < 1:
-                     self.objMan.ManualControlCommand.Roll.value += 0.05
-                     self.objMan.ManualControlCommand.updated()
-                     time.sleep(0.1)
-
-                if self.objMan.ManualControlCommand.Roll.value > 1:
-                    self.objMan.ManualControlCommand.Roll.value = 0
-                    self.objMan.ManualControlCommand.updated()
-                    time.sleep(0.1)
-
-            self.objMan.ManualControlCommand.Roll.value = 0
-            self.objMan.ManualControlCommand.updated()
-            time.sleep(0.5)
-
-            self.eprint("Setting Throttle to minimum")
-            self.objMan.ManualControlCommand.Throttle.value = -1
-            self.objMan.ManualControlCommand.updated()
-            time.sleep(1)
-
-            self.__disarmBoard()
-        
-            self.__freeControl()
-            
         
     def stopDrone(self):
         self.terminate();
@@ -401,4 +403,17 @@ class UAVTalkDemo():
         self.eprint("Stoped Drone")
         
         return True
- 
+        
+if __name__ == "__main__":
+    try:
+        uavTalk = UAVTalkDemo(None)
+        uavTalk.setup("/dev/ttyS0")
+        print('setup finished')
+    except KeyboardInterrupt:
+        uavTalk.stop()
+        raise
+    else:
+        print threading.enumerate()
+        
+    #stop all:
+    uavTalk.stop()
